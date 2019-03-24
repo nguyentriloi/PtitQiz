e3a227c82b73531d6201c8a579ce8ac07d40641a
nguyentriloi.it@gmail.com <Meograce@123>
2019-03-24 15:49:54 +0700

    buid

---
+++ src/main/java/com/loiclude/PtitQuiz/PtitQuizApplication.java
@@ -0,0 +1,13 @@
+package com.loiclude.PtitQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtitQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtitQuizApplication.class, args);
+	}
+
+}
